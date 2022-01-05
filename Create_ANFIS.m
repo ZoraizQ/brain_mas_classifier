cdb139d95c1ffea15298dbfefb9e4a2e65f4d1d6
Zoraiz Qureshi <zce5py@virginia.edu>
2022-01-05 11:34:34 +0500

    Delete Create_ANFIS.m

--- Create_ANFIS.m
+++
@@ -1,7 +0,0 @@
-function [fis_Anfis,Out_Anfis,rmse] = Create_ANFIS(train_data, in_fis, epochs)
-
-    [fis_Anfis,rmse] = anfis(train_data, in_fis, epochs);
-    x=train_data(:,1:end-1);
-    Out_Anfis=evalfis(x,fis_Anfis);
-
-end
